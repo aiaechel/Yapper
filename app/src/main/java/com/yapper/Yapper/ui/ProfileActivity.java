bd7b72a8d79296068345d6f8e656abde31927150
Cronom <ericyang94@gmail.com>
2017-06-04 01:36:13 -0700

    Get profile data from database

--- app/src/main/java/com/yapper/Yapper/ui/ProfileActivity.java
+++ app/src/main/java/com/yapper/Yapper/ui/ProfileActivity.java
@@ -4,16 +4,50 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.TextView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.yapper.Yapper.R;
 import com.yapper.Yapper.utils.ChatRoom;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private DatabaseReference profile_root;
+    private TextView user_name_textview;
+    private TextView email_textview;
+    private TextView description_textview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        user_name_textview = (TextView) findViewById(R.id.UserNameText);
+        email_textview = (TextView) findViewById(R.id.EmailText);
+        description_textview = (TextView) findViewById(R.id.DescriptionText);
+
+        profile_root = FirebaseDatabase.getInstance().getReference().child("users").child("userID");
+
+        profile_root.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String user_name = (String) dataSnapshot.child("user_name").getValue();
+                user_name_textview.setText(user_name);
+
+                String email = (String) dataSnapshot.child("email").getValue();
+                email_textview.setText(email);
+
+                //String description = (String) dataSnapshot.child("").getValue();
+                //description_textview.setText(description);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) { }
+        });
     }
 
     public void sendMessage(View view){
