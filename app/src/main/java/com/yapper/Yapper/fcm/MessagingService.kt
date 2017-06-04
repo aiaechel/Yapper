edeeccda12e1adb334e4771b236b4e397beec98d
Kevin Zhu <kevinazhu@gmail.com>
2017-06-04 04:10:02 -0700

    added handling of received cloud messages

---
+++ app/src/main/java/com/yapper/Yapper/fcm/MessagingService.kt
@@ -0,0 +1,12 @@
+package com.yapper.Yapper.fcm
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        //todo: handle notification
+    }
+
+}
