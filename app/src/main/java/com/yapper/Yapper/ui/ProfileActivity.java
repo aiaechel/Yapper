c54d1021e6ac58b81bb10dea94f6e8914ff76a14
Cronom <ericyang94@gmail.com>
2017-06-04 00:12:59 -0700

    Profile Layout

---
+++ app/src/main/java/com/yapper/Yapper/ui/ProfileActivity.java
@@ -0,0 +1,15 @@
+package com.yapper.Yapper.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yapper.Yapper.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
