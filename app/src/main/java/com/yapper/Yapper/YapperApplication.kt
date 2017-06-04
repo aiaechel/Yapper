c6919ef9ef8096f263e4c79285add91c0b64ce3c
Andrew Lee <aiaechel@gmail.com>
2017-06-03 23:40:56 -0700

    Added location to Chatroom List

---
+++ app/src/main/java/com/yapper/Yapper/YapperApplication.kt
@@ -0,0 +1,19 @@
+package com.yapper.Yapper
+
+import android.app.Application
+import android.content.Context
+
+class YapperApplication : Application() {
+
+    companion object {
+
+        private lateinit var context : Context
+
+        fun getApplicationContext() = context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = this
+    }
+}
---
+++ app/src/main/java/com/yapper/Yapper/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.yapper.Yapper.utils
+
+object Constants {
+    const val BACKEND_URL = "https://us-central1-yapper-d1d04.cloudfunctions.net"
+}
