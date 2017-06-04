010bc7be369fdc1c6eaedd54716ba20e37fc6f0c
Cronom <ericyang94@gmail.com>
2017-06-04 02:35:23 -0700

    image url display

--- app/src/main/java/com/yapper/Yapper/ui/ProfileActivity.java
+++ app/src/main/java/com/yapper/Yapper/ui/ProfileActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.database.ChildEventListener;
@@ -12,6 +13,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 import com.yapper.Yapper.R;
 import com.yapper.Yapper.utils.ChatRoom;
 
@@ -21,6 +23,7 @@ public class ProfileActivity extends AppCompatActivity {
     private TextView user_name_textview;
     private TextView email_textview;
     private TextView description_textview;
+    private ImageView profile_image_imageview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,7 @@ public class ProfileActivity extends AppCompatActivity {
         user_name_textview = (TextView) findViewById(R.id.UserNameText);
         email_textview = (TextView) findViewById(R.id.EmailText);
         description_textview = (TextView) findViewById(R.id.DescriptionText);
+        profile_image_imageview = (ImageView) findViewById(R.id.ProfileImage);
 
         Bundle extras = getIntent().getExtras();
         String user_id = getIntent().getExtras().getString("user_id");
@@ -43,6 +47,11 @@ public class ProfileActivity extends AppCompatActivity {
 
                 String email = (String) dataSnapshot.child("email").getValue();
                 email_textview.setText(email);
+
+                String picture = (String) dataSnapshot.child("photo_url").getValue();
+                if (picture != null && !picture.isEmpty()) {
+                    Picasso.with(ProfileActivity.this).load(picture).into(profile_image_imageview);
+                }
 
                 //String description = (String) dataSnapshot.child("").getValue();
                 //description_textview.setText(description);
