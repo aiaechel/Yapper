4222426532972338abe1c7d4e642b2cb863d300d
jteo1 <jteo1@yahoo.com>
2017-06-03 19:55:59 -0700

    some small code refactoring, preparing to add userID -> username conversion

--- app/src/main/java/com/yapper/Yapper/utils/ChatRoom.java
+++ app/src/main/java/com/yapper/Yapper/utils/ChatRoom.java
@@ -3,6 +3,7 @@ package com.yapper.Yapper.utils;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.text.SpannableString;
 import android.text.Spanned;
@@ -24,6 +25,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.yapper.Yapper.R;
 import org.jetbrains.annotations.Nullable;
 import org.w3c.dom.Text;
@@ -43,7 +45,7 @@ public class ChatRoom extends AppCompatActivity {
     private Button btn_send_msg;
     private EditText input_msg;
     private ScrollView scroll_view;
-    private DatabaseReference root;
+    private DatabaseReference chatrooms_root;
     private String temp_key;
 
     private String user_id, room_id;
@@ -61,21 +63,21 @@ public class ChatRoom extends AppCompatActivity {
         user_id = "userID";
         room_id = "halp";
 
-        root = FirebaseDatabase.getInstance().getReference().child("chatrooms").child(room_id).child("messages");
+        chatrooms_root = FirebaseDatabase.getInstance().getReference().child("chatrooms").child(room_id).child("messages");
 
+        // TODO: extract actual room name from room_id
         setTitle(room_id);
 
         btn_send_msg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Map<String, Object> messages = new HashMap<String, Object>();
-                temp_key = root.push().getKey();
-                root.updateChildren(messages);
+                temp_key = chatrooms_root.push().getKey();
+                chatrooms_root.updateChildren(messages);
 
-                DatabaseReference message_root = root.child(temp_key);
+                DatabaseReference message_root = chatrooms_root.child(temp_key);
                 Map<String, Object> name_and_message = new HashMap<String, Object>();
                 name_and_message.put("username", user_id);
-                //TODO: generate timestamp
                 name_and_message.put("timestamp", DateFormat.getDateTimeInstance().format(new Date()));
                 name_and_message.put("body", input_msg.getText().toString());
 
@@ -86,7 +88,7 @@ public class ChatRoom extends AppCompatActivity {
             }
         });
 
-        root.addChildEventListener(new ChildEventListener() {
+        chatrooms_root.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 append_chat(dataSnapshot);
@@ -114,33 +116,51 @@ public class ChatRoom extends AppCompatActivity {
         });
     }
 
-    private String chat_msg, chat_user_name, timestamp;
 
+
+    //append a single message to the message stream
     private void append_chat(DataSnapshot dataSnapshot) {
-        Iterator i = dataSnapshot.getChildren().iterator();
-        while(i.hasNext()) {
-            //get body, timestamp, and user in that order
-            chat_msg = (String) ((DataSnapshot)i.next()).getValue();
-            timestamp = (String) ((DataSnapshot)i.next()).getValue();
-            chat_user_name = (String) ((DataSnapshot)i.next()).getValue();
+        String chat_msg, chat_user_id, timestamp, chat_user_name;
 
-            TextView text_view = new TextView(ChatRoom.this);
+        chat_msg = (String) dataSnapshot.child("body").getValue();
+        timestamp = (String) dataSnapshot.child("timestamp").getValue();
+        chat_user_id = (String) dataSnapshot.child("username").getValue();
+        chat_user_name = "null?";
 
-            //make user name clickable to go to their profile page
-            SpannableString ss = new SpannableString(chat_user_name + " (" + timestamp + "):\n" + chat_msg);
-            ss.setSpan(new MyClickableSpan(), 0, chat_user_name.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        /*
+        //retrieve username from user_id
+        FirebaseDatabase.getInstance().getReference().child("users").child(chat_user_id).child("display_name").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.wtf("hi", (String)dataSnapshot.getValue());
+                chat_user_name = (String) dataSnapshot.getValue();
 
-            //TODO: update UI for message stream
-            text_view.setText(ss);
-            text_view.setMovementMethod(LinkMovementMethod.getInstance());
-            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            lp.setMargins(0, 0, 0, 10);
-            text_view.setLayoutParams(lp);
-            text_view.setBackgroundColor(Color.parseColor("#dddddd"));
+            }
 
-            layout.addView(text_view);
-            scroll_view.fullScroll(View.FOCUS_DOWN);
-        }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        */
+
+        TextView text_view = new TextView(ChatRoom.this);
+
+        //make user name clickable to go to their profile page
+        SpannableString ss = new SpannableString(chat_user_name + " (" + timestamp + "):\n" + chat_msg);
+        ss.setSpan(new MyClickableSpan(), 0, chat_user_name.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        //TODO: update UI for message stream
+        text_view.setText(ss);
+        text_view.setMovementMethod(LinkMovementMethod.getInstance());
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(0, 0, 0, 10);
+        text_view.setLayoutParams(lp);
+        text_view.setBackgroundColor(Color.parseColor("#dddddd"));
+
+        layout.addView(text_view);
+        scroll_view.fullScroll(View.FOCUS_DOWN);
     }
 
 
